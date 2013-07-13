8827391172e4ef17eddbf79f57ab708661143091
Luke <ldunekacke@gmail.com>
2013-07-13 16:38:00 -0600

    added showBaordCell and refactored names of functions

--- src/stratego.hs
+++ src/stratego.hs
@@ -10,9 +10,10 @@ data Square = Piece PColor PType | NoEntry deriving (Show)
 data PColor = Blue | Red deriving (Show)
 data PType = Flag | Bomb | Spy | Scout | Miner | General | Marshal deriving (Show)
 
+
+
 showBoardCell :: BoardCell -> Char
-showBoardCell Nothing   = ' '
-showBoardCell (Just a)  = showSquare a      
+showBoardCell = maybe ' ' showSquare    
 
 showSquare :: Square -> Char
 showSquare (Piece _ Flag)    = 'F'
@@ -24,24 +25,24 @@ showSquare (Piece _ General) = '9'
 showSquare (Piece _ Marshal) = '0'
 showSquare NoEntry           = 'W'
 
-readPiece :: Maybe PColor ->Char -> Square
-readPiece (Just Blue) 'F' = (Piece Blue Flag)  
-readPiece (Just Blue) 'B' = (Piece Blue Bomb)    
-readPiece (Just Blue) '1' = (Piece Blue Spy)     
-readPiece (Just Blue) '2' = (Piece Blue Scout)   
-readPiece (Just Blue) '3' = (Piece Blue Miner)   
-readPiece (Just Blue) '9' = (Piece Blue General) 
-readPiece (Just Blue) '0' = (Piece Blue Marshal) 
+readSquare :: Maybe PColor ->Char -> Square
+readSquare (Just Blue) 'F' = (Piece Blue Flag)  
+readSquare (Just Blue) 'B' = (Piece Blue Bomb)    
+readSquare (Just Blue) '1' = (Piece Blue Spy)     
+readSquare (Just Blue) '2' = (Piece Blue Scout)   
+readSquare (Just Blue) '3' = (Piece Blue Miner)   
+readSquare (Just Blue) '9' = (Piece Blue General) 
+readSquare (Just Blue) '0' = (Piece Blue Marshal) 
 
-readPiece (Just Red)  'F' = (Piece Red Flag)    
-readPiece (Just Red)  'B' = (Piece Red Bomb)    
-readPiece (Just Red)  '1' = (Piece Red Spy)     
-readPiece (Just Red)  '2' = (Piece Red Scout)   
-readPiece (Just Red)  '3' = (Piece Red Miner)   
-readPiece (Just Red)  '9' = (Piece Red General) 
-readPiece (Just Red)  '0' = (Piece Red Marshal)
+readSquare (Just Red)  'F' = (Piece Red Flag)    
+readSquare (Just Red)  'B' = (Piece Red Bomb)    
+readSquare (Just Red)  '1' = (Piece Red Spy)     
+readSquare (Just Red)  '2' = (Piece Red Scout)   
+readSquare (Just Red)  '3' = (Piece Red Miner)   
+readSquare (Just Red)  '9' = (Piece Red General) 
+readSquare (Just Red)  '0' = (Piece Red Marshal)
 
-readPiece Nothing 'W' = NoEntry
+readSquare Nothing 'W' = NoEntry
 -- Tests
 
 tests = TestList $ map TestCase
